3663a024180a1437414cbb4f7ba724153149d97b
Lakshmi Narasimhan <badri.dilbert@gmail.com>
2023-08-08 08:47:30 +0530

    Update env vars.

--- web/sites/default/settings.shapeblock.php
+++ web/sites/default/settings.shapeblock.php
@@ -2,10 +2,10 @@
 
   $databases['default']['default'] = [
     'driver' => 'mysql',
-    'database' => getenv('MYSQL_DATABASE'),
-    'username' => getenv('MYSQL_USER'),
-    'password' => getenv('MYSQL_PASSWORD'),
-    'host' => getenv('MYSQL_HOST'),
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'host' => getenv('DB_HOST'),
     'port' => '3306',
     'init_commands' => [
       'isolation_level' => 'SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED',
